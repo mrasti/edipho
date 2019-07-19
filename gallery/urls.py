fa31c8067a852c58dc1ba33fa70b189cb7e407e5
Melika <melika.r@gmail.com>
2019-07-19 15:37:36 -0400

    added login page

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.photo_list, name = 'photo_list'),
     path('photo/<int:pk>/update', views.photo_update, name = 'photo_update'),
+    path('login/', LoginView.as_view(template_name='login.html'), name= 'LoginView'),
 ]
