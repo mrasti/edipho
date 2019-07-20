193615542d714bc057bf1e88ccd5c735e745e7af
Melika <melika.r@gmail.com>
2019-07-20 17:47:55 -0400

    added style for login and signup

--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+# from django import forms
+# from django.forms import ModelForm
 
 
 class Photo(models.Model):
     title = models.CharField(default='', max_length = 300, blank=True, null=True)
+    image = models.ImageField(blank = False, null = False)
     location = models.CharField(default='', max_length = 300, blank=True, null=True)
     time_uploaded = models.DateTimeField(auto_now_add = True, auto_now = False)
-    image = models.ImageField(blank = False, null = False)
     filter = models.TextField(default='', blank=True, null=True)
 
     def __str__(self):
